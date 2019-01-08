77e1fd6b0771ad2feaea1db998959fd937befd17
SikindarMirza <sikindar247@gmail.com>
2019-01-08 15:15:51 +0530

    Basic buttons using styled components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled, { css } from 'styled-components';
+import { ENETDOWN } from 'constants';
+
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 1rem 1em;
+  padding: 1em 1em;
+  ${props => props.primary && css`
+    background: palevioletred;
+    color: #fff;
+  `}
+`
+const BlueBotton = styled(Button)`
+  color: blue;
+  border: 2px solid blue;
+`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* <header className="App-header">
+         
+        </header> */}
+        <Button primary>I'm a styled Primary Button!</Button>
+        <Button>I'm a styled Button!</Button>
+        <BlueBotton>Blue button</BlueBotton>
+
       </div>
     );
   }
 }
 
 export default App;
+
+
